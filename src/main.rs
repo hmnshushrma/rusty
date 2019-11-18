c46a5f6316ae1fa7bcd692707e8f89c8e5de6286
himanshu-adcat <himanshus45@hdfclife.com>
2019-11-19 00:49:09 +0530

    understanding dependency

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
-use std::io;
+use std::io; // capture user's input and provides outupt to cli
+use rand::Rng; // random number dependency held by cargo , cargo is a package manager just like npm
 
 fn main(){
     println!("Guess the number");
+
+    let  secret_number = rand::thread_rng().gen_range(1,100);
+
+    println!("Secret number is  :{}", secret_number);
+
     println!("Please input your guess");
 
     let mut guess = String::new(); //def for mutable variable
-    io::stdin().read_line(&mut guess).expect("failed to read the line");
-    println!("You  Guessed:{}", guess);
+    io::stdin().read_line(&mut guess)
+        .expect("failed to read the line"); //error handling
+
+    println!("You  Guessed:{}", guess); // {} is a place holder
+
+
 }
